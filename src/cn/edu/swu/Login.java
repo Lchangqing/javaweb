6c718eded6407c8eb21994eb93013f611021f710
leichangqing <1365131604@qq.com>
2019-03-26 01:33:48 +0800

    second time conmmit

--- src/cn/edu/swu/Login.java
+++ src/cn/edu/swu/Login.java
@@ -1,37 +1,96 @@
 package cn.edu.swu;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class Login extends HttpServlet{
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-	  public void init() throws ServletException
-	  {
-	      // 执行必需的初始化
-	  }
+public class Login extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Connection con=null;
+	PreparedStatement ps=null; 
+	ResultSet rs=null;
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 response.setCharacterEncoding("UTF-8") ;
+		String username = (String) request.getParameter("user");
+		String password = (String) request.getParameter("pass");
+		System.out.println(username);
 
-	  public void doPost(HttpServletRequest request,
-	                    HttpServletResponse response)
-	            throws ServletException, IOException
-	  {
-		  String username=(String)request.getParameter("user");
-		  String password=(String)request.getParameter("pass");
-		  System.out.println(username);
-		  System.out.println(password);
-		  
-	      PrintWriter out = response.getWriter();
-	      if(username.equals("leichangqing")&&password.equals("123456"))
-	      {
-	    	  out.println("<h1>Login success</h1>");
-	      }else {
-	    	  out.println("<h1>Login fail</h1>");
-	      }
-	  }
-	  
-	  public void destroy()
-	  {
-	      // 什么也不做
-	  }
+		PrintWriter out = response.getWriter();
+		
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/demo01?useSSL=false&characterEncoding=utf8","root","123456");
+			 System.out.println("连接成功");
+			String sql="select * from userinfo where name=? and passwd=?";
+			ps=con.prepareStatement(sql);
+			ps.setObject(1,username);
+			ps.setObject(2,password);
+			rs=ps.executeQuery();
+			 System.out.println("准备进入if");
+			if(rs.next()) {
+			 System.out.println("成功进入if");
+				int count=rs.getInt(1);
+				if(count>0) {
+//					System.out.println(rs.getInt(1)+"----------"+rs.getString(2)+"-----------"+rs.getString(3));
+//					out.println("login success!!");
+					request.getRequestDispatcher("/second.jsp").forward(request, response);
+				}
+			}else{
+				out.println("login failed!!");
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null) {
+					rs.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				if(ps!=null) {
+					ps.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				if(con!=null) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	}
+	
+	
+	
+	
 
-}
+
--- web/index.jsp
+++ web/index.jsp
@@ -1,9 +1,19 @@
-<center>
-<form action="hello" method="POST">
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8 ">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="hello" method="POST">
 
-user:<input type="text" name="user"><br/>
-pass:<input type="password" name="pass"><br/>
-<input type="submit" name="submit">
+		user:<input type="text" name="user"><br />
+	    pass:<input	type="password" name="pass"><br />
+	         <input type="submit"name="submit">
 
-</form>
-</center>
+	</form>
+
+</body>
+</html>
